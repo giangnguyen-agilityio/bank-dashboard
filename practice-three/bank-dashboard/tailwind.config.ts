2d53dcc77be15a6cfd1dac7272afdb3a570d2530
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-09-16 17:16:42 +0700

    feat: configure font styles used in the project

--- practice-three/bank-dashboard/tailwind.config.ts
+++ practice-three/bank-dashboard/tailwind.config.ts
@@ -1,13 +1,22 @@
-import type { Config } from "tailwindcss";
-import { nextui } from "@nextui-org/theme";
+import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/theme';
 
 const config: Config = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Inter', 'sans-serif'],
+        secondary: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+
+  darkMode: 'class',
   plugins: [nextui()],
 };
 export default config;
