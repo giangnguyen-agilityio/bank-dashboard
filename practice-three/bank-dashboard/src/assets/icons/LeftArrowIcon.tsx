86e1c9231cccfee52084fc64d7d27f9ec1a91557
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-09-20 12:56:43 +0700

    feat: update the icon components

--- practice-three/bank-dashboard/src/assets/icons/LeftArrowIcon.tsx
+++ practice-three/bank-dashboard/src/assets/icons/LeftArrowIcon.tsx
@@ -10,6 +10,6 @@ export const LeftArrowIcon = ({
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M8 13L2 7L8 1" stroke="currentColor" stroke-width="2" />
+    <path d="M8 13L2 7L8 1" stroke="currentColor" strokeWidth="2" />
   </svg>
 );
--- practice-three/bank-dashboard/src/assets/icons/RightArrowIcon.tsx
+++ practice-three/bank-dashboard/src/assets/icons/RightArrowIcon.tsx
@@ -10,6 +10,6 @@ export const RightArrowIcon = ({
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M1 1L7.5 7.5L1 14" stroke="currentColor" stroke-width="2" />
+    <path d="M1 1L7.5 7.5L1 14" stroke="currentColor" strokeWidth="2" />
   </svg>
 );
