38da6315d37cb8d0ba0b1002f0fa93b05b705580
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-09-27 02:36:05 +0700

    feat: update the unit test for the CustomTabs component

---
+++ practice-three/bank-dashboard/src/components/common/Tabs/__test__/index.test.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import { render, screen } from '@testing-library/react';
+
+// Mock
+import { MOCK_TAB_DATA } from '@app/mocks';
+
+// Components
+import { CustomTabs } from '@app/components';
+
+describe('CustomTabs Component', () => {
+  it('should renders the CustomTabs correctly without crashing', () => {
+    render(
+      <CustomTabs
+        tabs={MOCK_TAB_DATA}
+        selectedKey="login"
+        onSelectionChange={() => {}}
+      />,
+    );
+
+    expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Email')).toBeInTheDocument();
+  });
+
+  it('should not renders the CustomTabs when the tabs is empty', () => {
+    render(
+      <CustomTabs
+        tabs={
+          undefined as unknown as {
+            key: string | number | null;
+            title: string;
+            tabContent: ReactNode;
+          }[]
+        }
+        selectedKey="login"
+        onSelectionChange={() => {}}
+      />,
+    );
+
+    expect(screen.queryByTestId('custom-tab-item')).not.toBeInTheDocument();
+  });
+});
