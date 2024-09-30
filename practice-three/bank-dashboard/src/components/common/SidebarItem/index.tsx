dc26299b46a2c810066898dde4dae8efd13d478e
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-09-30 18:53:58 +0700

    feat: create the SidebarItem component

---
+++ practice-three/bank-dashboard/src/components/common/SidebarItem/index.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from 'react';
+import { Link, useLocation } from '@tanstack/react-router';
+
+// Constants
+import { DESTINATION } from '@app/constants';
+
+interface SidebarItemProps {
+  icon: ReactNode;
+  label: string;
+  link: string;
+  onToggleSidebar: () => void;
+}
+
+const SidebarItem = ({
+  icon,
+  label,
+  link,
+  onToggleSidebar,
+}: SidebarItemProps) => {
+  const location = useLocation();
+
+  const isActive =
+    location.pathname === link ||
+    (location.pathname === '/' && link === DESTINATION.DASHBOARD);
+
+  return (
+    <li
+      data-testid="sidebar-item"
+      className={`pl-7.5 py-3.75 lg:py-4.5 rounded-r-3xl border-l-5 ${
+        isActive ? 'border-blue-200' : 'border-transparent'
+      }`}
+      onClick={onToggleSidebar}
+    >
+      <Link
+        to={link}
+        className={`flex items-center transition-colors ${
+          isActive ? 'text-blue-200' : 'text-gray-100'
+        } hover:text-blue-200`}
+        role="menuitem"
+      >
+        {icon}
+        <span className="ml-5 lg:ml-6.5 font-primary font-medium text-2xl lg:text-4xl">
+          {label}
+        </span>
+      </Link>
+    </li>
+  );
+};
+
+export default SidebarItem;
