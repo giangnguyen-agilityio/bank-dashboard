d0dd8eca6c88beb8759278fed1fc0b13b2c9f6af
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-10-02 18:35:15 +0700

    fix: update the Login page style

--- practice-three/bank-dashboard/src/pages/LoginPage/index.tsx
+++ practice-three/bank-dashboard/src/pages/LoginPage/index.tsx
@@ -5,36 +5,34 @@ import { Card, CardBody } from '@nextui-org/react';
 import { LogoIcon } from '@app/assets';
 
 // Components
-import { Box, Container, LoginForm, Text } from '@app/components';
+import { Box, LoginForm, Text } from '@app/components';
 
 const LoginPage = () => {
   return (
-    <Container className="font-primary bg-background-primary">
-      <Box className="min-h-screen">
-        {/* Logo */}
-        <Link
-          to="/"
-          as="h1"
-          className="flex items-center cursor-pointer h-20.5 lg:h-25 hover:opacity-100 active:opacity-100"
-          aria-label="Logo"
-        >
-          <LogoIcon customClass="w-full text-text-secondary" />
-        </Link>
+    <Box className="min-h-screen">
+      {/* Logo */}
+      <Link
+        to="/"
+        as="h1"
+        className="flex items-center cursor-pointer h-20.5 lg:h-25 hover:opacity-100 active:opacity-100"
+        aria-label="Logo"
+      >
+        <LogoIcon customClass="w-full text-text-secondary" />
+      </Link>
 
-        <Card className="w-full max-w-md mx-auto">
-          <CardBody className="p-7 rounded-2xl bg-background-default">
-            <Text
-              as="h2"
-              variant="heading"
-              customClass="uppercase text-center mb-5 text-2xl"
-            >
-              Sign in
-            </Text>
-            <LoginForm />
-          </CardBody>
-        </Card>
-      </Box>
-    </Container>
+      <Card className="w-full max-w-md mx-auto">
+        <CardBody className="p-7 rounded-2xl bg-background-default">
+          <Text
+            as="h2"
+            variant="heading"
+            customClass="uppercase text-center mb-5 text-2xl"
+          >
+            Sign in
+          </Text>
+          <LoginForm />
+        </CardBody>
+      </Card>
+    </Box>
   );
 };
 
