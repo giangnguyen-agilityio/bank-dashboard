75343b273aeb9652eee905df8f518a4adfd41f8d
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-10-02 12:24:50 +0700

    fix: remove the redundant code in services

--- practice-three/bank-dashboard/src/services/httpClient.ts
+++ practice-three/bank-dashboard/src/services/httpClient.ts
@@ -1,11 +1,4 @@
 import axios from 'axios';
-import { useNavigate } from '@tanstack/react-router';
-
-// Constants
-import { DESTINATION, ERROR_MESSAGE } from '@app/constants';
-
-// Utils
-import { convertDateToTimestamp } from '@app/utils';
 
 const httpClient = axios.create({
   baseURL: process.env.VITE_APP_API_URL,
@@ -13,34 +6,5 @@ const httpClient = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-httpClient.interceptors.request.use(
-  (config) => {
-    const exp = localStorage.getItem('exp');
-    const navigate = useNavigate();
-
-    if (exp) {
-      const currentDay = convertDateToTimestamp(exp);
-
-      if (Date.now() >= currentDay) {
-        localStorage.clear();
-        navigate({ to: DESTINATION.LOGIN });
-
-        return Promise.reject(ERROR_MESSAGE.SESSION_HAS_EXPIRED);
-      }
-    }
-    return config;
-  },
-  (error) => {
-    console.log(error);
-
-    return Promise.reject(error);
-  },
-);
-
-httpClient.interceptors.response.use(
-  (response) => response,
-  (error) => Promise.reject(error),
-);
 
 export { httpClient };
