9e8b125f8e12b1bfb30240de4c53e5d939f05db6
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-10-02 18:32:18 +0700

    fix: update the table constant

--- practice-three/bank-dashboard/src/constants/table.ts
+++ practice-three/bank-dashboard/src/constants/table.ts
@@ -1,3 +1,6 @@
+import { TransactionData } from '@app/interfaces';
+import { TableColumnType } from '@app/types';
+
 // The config width for column in table
 const WIDTH_COLUMN_CONFIG = {
   SMALL: 80,
@@ -8,4 +11,48 @@ const WIDTH_COLUMN_CONFIG = {
 // Default size of table columns
 const SIZE_COLUMN_DEFAULT = 120;
 
-export { WIDTH_COLUMN_CONFIG, SIZE_COLUMN_DEFAULT };
+const TRANSACTION_TABLE_TABS = {
+  ALL_TRANSACTIONS: {
+    KEY: 'all-transactions',
+    TITLE: 'All Transactions',
+  },
+  EXPENSE_TRANSACTIONS: {
+    KEY: 'expense',
+    TITLE: 'Expense',
+  },
+  INCOME_TRANSACTIONS: {
+    KEY: 'income',
+    TITLE: 'Income',
+  },
+};
+
+const COLUMNS_TRANSACTION_LIST: TableColumnType<TransactionData>[] = [
+  { header: 'Description', accessor: 'description' },
+  {
+    header: 'Transaction ID',
+    accessor: 'id',
+  },
+  {
+    header: 'Type',
+    accessor: 'category',
+  },
+  {
+    header: 'Card',
+    accessor: 'cardNumber',
+  },
+  {
+    header: 'Date',
+    accessor: 'date',
+  },
+  {
+    header: 'Amount',
+    accessor: 'amount',
+  },
+];
+
+export {
+  WIDTH_COLUMN_CONFIG,
+  SIZE_COLUMN_DEFAULT,
+  TRANSACTION_TABLE_TABS,
+  COLUMNS_TRANSACTION_LIST,
+};
