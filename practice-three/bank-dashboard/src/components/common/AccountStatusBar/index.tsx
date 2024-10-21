df82e51f9054e4b247cd8f0f2514c3e31a485cb4
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2024-10-21 14:03:16 +0700

    feat: create the patch for the pagination nextui component

--- practice-three/bank-dashboard/src/components/common/AccountStatusBar/index.tsx
+++ practice-three/bank-dashboard/src/components/common/AccountStatusBar/index.tsx
@@ -16,6 +16,7 @@ const AccountStatusBar = ({ data = [] }: AccountStatusBarProps) => (
     aria-label="Account status bar"
     data-testid="account-status-bar"
     className="flex w-full overflow-auto bg-background-primary p-1 gap-5 md:gap-5.5 lg:gap-7.5"
+    tabIndex={0}
   >
     {data.length > 0 ? (
       data.map(({ icon, title, quantity, backgroundColor }) => (
